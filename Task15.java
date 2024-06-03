544dec8a07212b590f03b8d857c292429b87d297
Sowmyabala <167969513+Sowmya-21n@users.noreply.github.com>
2024-06-03 17:36:13 +0530

    Add files via upload

---
+++ Task15.java
@@ -0,0 +1,25 @@
+package task15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+    public class Task15
+    {
+
+        public static void main(String[] args)
+        {
+            //open the browser
+            ChromeDriver chromedriver = new ChromeDriver();
+            //Navigate to google website
+            chromedriver.get("https://google.com");
+            //locate id
+            By by = By.id("APjFqb");
+            //Find the by element
+            WebElement element = chromedriver.findElement(by);
+            //search for selenium browser driver
+            element.sendKeys("Selenium Browser Driver");
+            //click on enter
+            element.sendKeys(Keys.ENTER);
+        }
+    }
